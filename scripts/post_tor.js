dc89bd556e43547d245667d1a29b2ecb2de51271
Anapopo <favoursmoe@gmail.com>
2018-07-14 21:55:16 +0800

    Update: 重写文章目录算法

--- scripts/post_tor.js
+++ scripts/post_tor.js
@@ -1,24 +1,21 @@
 hexo.extend.helper.register('post_tor', function(content) {
-    let f = ""
-    let pattern = /<h[1-5] .*?>(.*?)<\/h[1-5]>/g
-    let tor_i = content.match(pattern)
+    let f = []
+    let header_pattern = /<h([1-5]) .*?<\/h[1-5]>/g
+    let data_pattern = /<h([1-5]) id="(.*?)"><a href=".*?" class="headerlink" title=".*?"><\/a>.*?<\/h[1-5]>/
+    let headers = content.match(header_pattern)
 
-    tor_i.map(tor => {
-        // console.log(tor)
-        let to = tor.replace(/<a .*?>.*?<\/a>/, "")
-        f += to
+    headers.map(header => {
+        let data = header.match(data_pattern)
+        if (data[1]!= undefined && data[2] != undefined)
+            f.push({ type: data[1], id: data[2]})
+        else throw Error("EmptyHeaderException")
     })
 
-    f = f.replace(/<h1 .*?>/g, `<span class="tori">`)
-    f = f.replace(/<\/h1>/g, `</span><br>`)
-    f = f.replace(/<h2 .*?>/g, `<span class="torii">`)
-    f = f.replace(/<\/h2>/g, `</span><br>`)
-    f = f.replace(/<h3 .*?>/g, `<span class="toriii">`)
-    f = f.replace(/<\/h3>/g, `</span><br>`)
-    f = f.replace(/<h4 .*?>/g, `<span class="toriiii">`)
-    f = f.replace(/<\/h4>/g, `</span><br>`)
-    f = f.replace(/<h5 .*?>/g, `<span class="toriiiii">`)
-    f = f.replace(/<\/h5>/g, `</span><br>`)
-    //this.log(f)
-    return `<p>${f}</p>`
+    //console.log(f) // 考虑做成API
+    let out = ""
+    for (one of f) {
+        let tor_class = 'tor' + 'i'.repeat(one.type)
+        out += `<a href="#${one.id}" class="${tor_class}">${one.id}</a><br>`
+    }
+    return out
 });
